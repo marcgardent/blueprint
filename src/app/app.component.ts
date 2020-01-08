5b00df1fe6add3fec53ec25b43a7deef4521250d
marcgardent <marcgardent@yahoo.fr>
2020-01-08 21:19:20 +0100

    feat(electron): read file from the fileSystem

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,20 +16,29 @@ export class AppComponent implements OnInit  {
 
   public context: BlueprintModel;
   private readonly electronWindow: Electron.BrowserWindow;
+  private readonly fs: any;
   
   constructor(private readonly electronService: ElectronService, private httpClient: HttpClient) {
     this.electronWindow = this.electronService.remote.getCurrentWindow();
+
+    this.fs = this.electronService.remote.require("fs").promises;
   }
 
   ngOnInit(): void {
     this.context = new BlueprintModel();
 
-   
-
-    //const self=this;
-    //const reader = new StandardFormatReader((x)=>self.httpClientLoader(x), this.context);
-    //reader.loadProject(this.context, "project.yml");
+    const self=this;
+    const reader = new StandardFormatReader((x)=>self.FileSystemLoader(x), this.context);
+    reader.loadProject(this.context, "myworkspace.yml");
   }
+
+  async FileSystemLoader(ref:string): Promise<any>{
+    const data = await this.fs.readFile("D:/tmp/blueprint/samples/py-protogame/"+ ref, "utf8");
+    const json = YAML.parse(data);
+    json.__file__ = ref;
+    return json;
+  }
+  
 
   async httpClientLoader(ref:string): Promise<any>{
     const data = await this.httpClient.get("assets/data/"+ ref, {responseType: 'text'}).toPromise();
