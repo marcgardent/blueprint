26cb193f61a6175ccf2ea2ac8cebebcb20576dd0
marcgardent <marcgardent@yahoo.fr>
2020-02-13 23:34:34 +0100

    unittest ready

---
+++ components/blueprint-core/src/io/writers/NunjucksWriter.spec.ts
@@ -0,0 +1,9 @@
+
+import "jasmine";
+import {NunjucksBuilder} from "./NunjucksWriter";
+
+describe("something", () => {
+    it("should work", () => {
+        expect(true).toBe(true);
+    });
+});
